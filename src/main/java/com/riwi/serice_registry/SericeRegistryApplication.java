1fe49c655672ad6e3d16421db6a2deed63e11574
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-07-22 12:16:23 -0500

    Add files via upload

---
+++ src/main/java/com/riwi/serice_registry/SericeRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.serice_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SericeRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SericeRegistryApplication.class, args);
+	}
+
+}
